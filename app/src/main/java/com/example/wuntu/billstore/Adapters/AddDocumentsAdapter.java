fd23b9e21ed51e2de062ceed0de1eafe66d251f9
Lakshya Punhani <lakshya.punhani@gmail.com>
2018-01-13 19:10:39 +0530

    Implemented File Picker Library

--- app/src/main/java/com/example/wuntu/billstore/Adapters/AddDocumentsAdapter.java
+++ app/src/main/java/com/example/wuntu/billstore/Adapters/AddDocumentsAdapter.java
@@ -1,14 +1,19 @@
 package com.example.wuntu.billstore.Adapters;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.wuntu.billstore.R;
 
 import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Dell on 1/10/2018.
@@ -17,9 +22,11 @@ import java.util.ArrayList;
 public class AddDocumentsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private final int VIEW_ITEM = 0;
-    private final int VIEW_PROG = 1;
+    private final int VIEW_FOOT = 1;
 
     private Context context;
+
+    boolean show_footer = true;
 
     private ArrayList<String> arrayList;
 
@@ -28,9 +35,14 @@ public class AddDocumentsAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         this.arrayList = arrayList;
     }
 
-    public class MyViewHolder1 extends RecyclerView.ViewHolder {
+    public class MyViewHolder1 extends RecyclerView.ViewHolder
+    {
+        @BindView(R.id.imageView)
+        ImageView imageView;
+
         public MyViewHolder1(View view) {
             super(view);
+            ButterKnife.bind(this,view);
 
         }
     }
@@ -43,16 +55,24 @@ public class AddDocumentsAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
     @Override
     public int getItemViewType(int position) {
-            if (isPositionItem(position))
-                return VIEW_ITEM;
-            return VIEW_PROG;
 
-
+        if (show_footer)
+        {
+            if (isPositionFooter(position))
+                return VIEW_FOOT;
+            return VIEW_ITEM;
+        }
+        else return VIEW_ITEM;
 
     }
 
-    private boolean isPositionItem(int position) {
-        return position != getItemCount() -1;
+    private boolean isPositionFooter(int position) {
+        return position == this.arrayList.size () ;
+    }
+
+    public void hideFooter()
+    {
+        show_footer = false;
     }
 
 
@@ -67,7 +87,7 @@ public class AddDocumentsAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
                     .inflate(R.layout.add_document_item, parent, false);
             return new MyViewHolder1(v);
 
-        } else if (viewType == VIEW_PROG){
+        } else if (viewType == VIEW_FOOT){
             View v = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.footer_add_document, parent, false);
             return new FooterViewHolder1(v);
@@ -77,12 +97,23 @@ public class AddDocumentsAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position)
+    {
+        if (holder instanceof MyViewHolder1)
+        {
+            String path = arrayList.get(position );
+            Uri uri = Uri.parse(path);
+            ((MyViewHolder1)holder).imageView.setImageURI(uri);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        if (show_footer)
+        {
+            return this.arrayList.size() + 1;
+        }
+        else return this.arrayList.size();
+
     }
 }
--- app/src/main/java/com/example/wuntu/billstore/AddNewBillActivity.java
+++ app/src/main/java/com/example/wuntu/billstore/AddNewBillActivity.java
@@ -1,13 +1,26 @@
 package com.example.wuntu.billstore;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.ParseException;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.transition.TransitionManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
@@ -19,9 +32,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.wuntu.billstore.Adapters.AddDocumentsAdapter;
+import com.example.wuntu.billstore.Utils.MarshMallowPermission;
+import com.example.wuntu.billstore.Utils.RecyclerViewListener;
 import com.example.wuntu.billstore.Utils.SearchableSpinner;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -32,6 +52,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
+import droidninja.filepicker.FilePickerBuilder;
+import droidninja.filepicker.FilePickerConst;
+import droidninja.filepicker.utils.Orientation;
 
 public class AddNewBillActivity extends AppCompatActivity {
 
@@ -39,8 +62,6 @@ public class AddNewBillActivity extends AppCompatActivity {
 
     @BindView(R.id.recycler_view)
     RecyclerView recycler_view;
-
-    ArrayList<String> arrayList;
 
     @BindView(R.id.vendorSpinner)
     SearchableSpinner vendorSpinner;
@@ -68,6 +89,15 @@ public class AddNewBillActivity extends AppCompatActivity {
 
     Calendar myCalendar = Calendar.getInstance();
 
+    ArrayList<String> arrayList;
+
+    ArrayList<String> recyclerViewList;
+
+    private int REQUEST_CAMERA = 0;
+
+    private int MAX_ATTACHMENT_COUNT = 6;
+
+    Uri selectedImageURI;
 
 
     @Override
@@ -78,22 +108,176 @@ public class AddNewBillActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        recyclerViewList = new ArrayList<>();
 
         arrayList = new ArrayList<>();
 
-        addDocumentsAdapter = new AddDocumentsAdapter(arrayList);
+        addDocumentsAdapter = new AddDocumentsAdapter(recyclerViewList);
         RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.HORIZONTAL);
         recycler_view.setLayoutManager(mLayoutManager);
         recycler_view.setItemAnimator(new DefaultItemAnimator());
         recycler_view.setNestedScrollingEnabled(false);
         recycler_view.setAdapter(addDocumentsAdapter);
 
+        recycler_view.addOnItemTouchListener(
+        new RecyclerViewListener(this, recycler_view, new RecyclerViewListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position)
+            {
+                final CharSequence[] options = {"Take Photo", "Gallery", "Document" ,  "Cancel"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(AddNewBillActivity.this);
+                builder.setTitle("Add Document!");
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (options[item].equals("Take Photo")) {
+
+
+                            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+
+                                cameraIntent();
+                                //Toast.makeText(AddNewBillActivity.this, "Take Photo Clicked", Toast.LENGTH_SHORT).show();
+
+                            } else {
+
+                                MarshMallowPermission marshMallowPermission = new MarshMallowPermission(AddNewBillActivity.this);
+                                if (!marshMallowPermission.checkPermissionForCamera()) {
+                                    marshMallowPermission.requestPermissionForCamera();
+                                } else {
+                                    if (!marshMallowPermission.checkPermissionForExternalStorage()) {
+                                        marshMallowPermission.requestPermissionForExternalStorage();
+                                    } else {
+                                        cameraIntent();
+                                        //Toast.makeText(AddNewBillActivity.this, "Take Photo Clicked", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            }
+
+
+                        } else if (options[item].equals("Gallery")) {
+
+                            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                                galleryPicker();
+                                //Toast.makeText(AddNewBillActivity.this, "Gallery Clicked", Toast.LENGTH_SHORT).show();
+                            } else {
+
+                                MarshMallowPermission marshMallowPermission = new MarshMallowPermission(AddNewBillActivity.this);
+
+                                if (!marshMallowPermission.checkPermissionForExternalStorage()) {
+                                    marshMallowPermission.requestPermissionForExternalStorage();
+                                } else {
+                                    //Toast.makeText(AddNewBillActivity.this, "Gallery Clicked", Toast.LENGTH_SHORT).show();
+                                    galleryPicker();
+
+                                }
+
+
+                            }
+                        }
+                        else if (options[item].equals("Document")) {
+                            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                                //galleryIntent();
+                                Toast.makeText(AddNewBillActivity.this, "Document Clicked", Toast.LENGTH_SHORT).show();
+                            } else {
+
+                                MarshMallowPermission marshMallowPermission = new MarshMallowPermission(AddNewBillActivity.this);
+
+                                if (!marshMallowPermission.checkPermissionForExternalStorage()) {
+                                    marshMallowPermission.requestPermissionForExternalStorage();
+                                } else {
+                                    //galleryIntent();
+                                    Toast.makeText(AddNewBillActivity.this, "Document Clicked", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        }
+                        else if (options[item].equals("Cancel")) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+                builder.show();
+                //Toast.makeText(AddNewBillActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+        }));
 
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayList);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         vendorSpinner.setAdapter(spinnerAdapter);
 
     }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+
+            if (requestCode == REQUEST_CAMERA) {
+
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+
+
+                File destination = new File(Environment.getExternalStorageDirectory(),
+                        System.currentTimeMillis() + ".jpg");
+
+                FileOutputStream fo;
+                try {
+                    destination.createNewFile();
+                    fo = new FileOutputStream(destination);
+                    fo.write(bytes.toByteArray());
+                    fo.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                String path = MediaStore.Images.Media.insertImage(this.getContentResolver(), thumbnail, "Title", null);
+
+                selectedImageURI = Uri.parse(path);
+                String selectedImage = selectedImageURI.toString();
+                recyclerViewList.add(selectedImage);
+
+                if (recyclerViewList.size() >= 6)
+                {
+                    addDocumentsAdapter.hideFooter();
+                }
+                addDocumentsAdapter.notifyDataSetChanged();
+
+
+            }
+
+            else if (requestCode == FilePickerConst.REQUEST_CODE_PHOTO)
+            {
+                if(data!=null)
+                {
+                    recyclerViewList.addAll(data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA));
+                    if (recyclerViewList.size() >= 6)
+                    {
+                        addDocumentsAdapter.hideFooter();
+                    }
+                    addDocumentsAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+    }
+
+
+
+
 
 
     DatePickerDialog.OnDateSetListener mDateListener = new DatePickerDialog.OnDateSetListener() {
@@ -156,6 +340,32 @@ public class AddNewBillActivity extends AppCompatActivity {
     public void onPaidRadioButtonClicked()
     {
         Toast.makeText(this, "Radio Paid Checked", Toast.LENGTH_SHORT).show();
+    }
+
+    private void cameraIntent()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    private void galleryPicker()
+    {
+        int maxCount = MAX_ATTACHMENT_COUNT - recyclerViewList.size();
+
+        if((recyclerViewList.size())==MAX_ATTACHMENT_COUNT)
+            Toast.makeText(this, "Cannot select more than " + MAX_ATTACHMENT_COUNT + " items", Toast.LENGTH_SHORT).show();
+        else
+        {
+            FilePickerBuilder.getInstance()
+                    .setMaxCount(maxCount)
+                    .setSelectedFiles(arrayList)
+                    .enableCameraSupport(false)
+                    .setActivityTheme(R.style.FilePickerTheme)
+                    .pickPhoto(this);
+        }
+
+
+
     }
 
 
