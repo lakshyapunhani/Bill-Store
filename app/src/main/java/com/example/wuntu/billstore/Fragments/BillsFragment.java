d0227213ac3f569ed89a83475c549bb6e3480ce2
Lakshya Punhani <lakshya.punhani@gmail.com>
2018-01-30 23:34:00 +0530

    Delete Documents from Firebase Storage Done

--- app/src/main/java/com/example/wuntu/billstore/Fragments/BillsFragment.java
+++ app/src/main/java/com/example/wuntu/billstore/Fragments/BillsFragment.java
@@ -2,16 +2,12 @@ package com.example.wuntu.billstore.Fragments;
 
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
@@ -22,26 +18,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.wuntu.billstore.Adapters.BillDocumentsAdapter;
-import com.example.wuntu.billstore.AddNewBillActivity;
 import com.example.wuntu.billstore.Pojos.BillDetails;
 import com.example.wuntu.billstore.R;
-import com.example.wuntu.billstore.Utils.MarshMallowPermission;
 import com.example.wuntu.billstore.Utils.RecyclerViewListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -93,6 +86,7 @@ public class BillsFragment extends Fragment {
     List<String> valuesList;
 
     DocumentReference billDateReference;
+    StorageReference mStorageRef;
 
 
     public BillsFragment() {
@@ -115,6 +109,7 @@ public class BillsFragment extends Fragment {
         db = FirebaseFirestore.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         billImages = new HashMap<>();
 
@@ -221,15 +216,36 @@ public class BillsFragment extends Fragment {
     @OnClick(R.id.btn_deleteDocument)
     public void deleteBill()
     {
-
         billDateReference.delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        //BillsFragment billsFragment = new BillsFragment();
-                        //getFragmentManager().beginTransaction().remove(billsFragment).commit();
-                        getActivity().getSupportFragmentManager().popBackStack();
-                        Toast.makeText(context, "Successfully Deleted", Toast.LENGTH_SHORT).show();
+                        StorageReference desertRef ;
+                        //desertRef = mStorageRef.child(firebaseUser.getUid()).child(vendorName);
+                        String parent = billDate + "&&" + billTime;
+                        for (int i = 0;i<keyList.size();i++)
+                        {
+                            desertRef = mStorageRef.child(firebaseUser.getUid()).child(vendorName).child(parent + "/" + keyList.get(i));
+                            final int finalI = i;
+                            desertRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    // File deleted successfully
+                                    if (finalI == keyList.size() - 1)
+                                    {
+                                        getActivity().getSupportFragmentManager().popBackStack();
+                                        Toast.makeText(context, "Successfully Deleted", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Uh-oh, an error occurred!
+                                }
+                            });
+                        }
+
+
                     }
                 }).addOnFailureListener(new OnFailureListener() {
             @Override
