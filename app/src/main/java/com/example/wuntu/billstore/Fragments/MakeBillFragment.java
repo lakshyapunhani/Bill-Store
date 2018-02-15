4f9216ae6d185e65842a99721a435d0d8080b1e9
Lakshya Punhani <lakshya.punhani@gmail.com>
2018-02-15 23:20:28 +0530

    Item Details Dialog added

--- app/src/main/java/com/example/wuntu/billstore/Fragments/MakeBillFragment.java
+++ app/src/main/java/com/example/wuntu/billstore/Fragments/MakeBillFragment.java
@@ -1,30 +1,66 @@
 package com.example.wuntu.billstore.Fragments;
 
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 
 import com.example.wuntu.billstore.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MakeBillFragment extends Fragment {
 
+    @BindView(R.id.btn)
+    Button btn;
+
+    private Context mContext;
+    Dialog dialog;
+
 
     public MakeBillFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_make_bill, container, false);
+        View view = inflater.inflate(R.layout.fragment_make_bill, container, false);
+        ButterKnife.bind(this,view);
+
+        dialog = new Dialog(mContext);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        View view1 = layoutInflater.inflate(R.layout.dialog_add_item, null);
+
+        dialog.setContentView(view1);
+
+        return view;
+    }
+
+    @OnClick(R.id.btn)
+    public void click()
+    {
+        dialog.show();
     }
 
 }
