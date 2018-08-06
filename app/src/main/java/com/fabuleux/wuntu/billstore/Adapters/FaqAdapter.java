edbb738063a51733d1976ffbfd692ad7b3ee6e4d
Lakshya <lakshya.punhani@gmail.com>
2018-08-06 23:29:05 +0530

    FAQ questions and answers added

--- app/src/main/java/com/fabuleux/wuntu/billstore/Adapters/FaqAdapter.java
+++ app/src/main/java/com/fabuleux/wuntu/billstore/Adapters/FaqAdapter.java
@@ -6,23 +6,37 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.fabuleux.wuntu.billstore.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class FaqAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 {
 
     private Context context;
+    String[] questions;
+    String[] answers;
 
     public FaqAdapter()
-    {
+    {}
 
+    public FaqAdapter(String[] questions, String[] answers)
+    {
+        this.questions = questions;
+        this.answers = answers;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder
     {
+        @BindView(R.id.question)
+        TextView question;
+
+        @BindView(R.id.answer)
+        TextView answer;
+
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this,itemView);
@@ -41,11 +55,12 @@ public class FaqAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position)
     {
-
+        ((ViewHolder)holder).answer.setText(answers[position]);
+        ((ViewHolder)holder).question.setText(questions[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return questions.length;
     }
 }
--- app/src/main/java/com/fabuleux/wuntu/billstore/FaqActivity.java
+++ app/src/main/java/com/fabuleux/wuntu/billstore/FaqActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import com.fabuleux.wuntu.billstore.Adapters.FaqAdapter;
 import com.fabuleux.wuntu.billstore.Adapters.ProductAdapter;
 
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -20,13 +22,26 @@ public class FaqActivity extends AppCompatActivity
 
     FaqAdapter faqAdapter;
 
+    String[] questions = {"How to generate invoice?"
+            ,"How to add products?"
+            ,"How to edit/delete product?"
+            ,"How to change language?"
+            ,"How to update personal details?"
+            ,"What is Add Bill?"};
+    String[] answers = {"Select make bill from bottom menu and select a customer or add new customer if no customer added yet by adding customer name, customer address and customer gst number if any. Select items to add in invoice by tapping on Invoiced Items and then preview the bill. Just print the bill. You can even see that bill generated under Customers on Home screen.",
+            "Select Settings from bottom menu and select Products. Tap the plus button in the bottom right corner and add product with product name,unit amount and description."
+            ,"Select Settings from bottom menu and select Products. Tap on the product and select edit or delete in the menu opened."
+            ,"Select Settings from bottom menu and select Change Language. Select the language and tap done."
+            ,"Select Settings from bottom menu and select Profile. Edit the fields and tap on update."
+            ,"Suppose you got a printed bill and you want to digitize it so add that bill with full information like bill amount,bill date, bill paid or due with picture of bill. Now you can access your bill in Bill Store under Vendors on Home screen."};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
         ButterKnife.bind(this);
 
-        faqAdapter = new FaqAdapter();
+        faqAdapter = new FaqAdapter(questions,answers);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(FaqActivity.this);
         faq_recycler_view.setLayoutManager(linearLayoutManager);
         faq_recycler_view.setAdapter(faqAdapter);
