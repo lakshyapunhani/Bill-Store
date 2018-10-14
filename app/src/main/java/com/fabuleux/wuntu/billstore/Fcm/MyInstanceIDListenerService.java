8080bea172314f09d019347fc359b9c649c02609
Lakshya Punhani <lakshya.punhani@gmail.com>
2018-10-14 15:00:17 +0530

    Network request added for registering device token

--- app/src/main/java/com/fabuleux/wuntu/billstore/Fcm/MyInstanceIDListenerService.java
+++ app/src/main/java/com/fabuleux/wuntu/billstore/Fcm/MyInstanceIDListenerService.java
@@ -1,8 +1,21 @@
 package com.fabuleux.wuntu.billstore.Fcm;
 
+import android.widget.Toast;
+
+import com.fabuleux.wuntu.billstore.Network.ApiClient;
+import com.fabuleux.wuntu.billstore.Network.ApiInterface;
 import com.freshchat.consumer.sdk.Freshchat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.util.HashMap;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Dell on 6/8/16.
@@ -22,8 +35,47 @@ public class MyInstanceIDListenerService extends FirebaseInstanceIdService {
         assert refreshedToken != null;
         Freshchat.getInstance(this).setPushRegistrationToken(refreshedToken);
         // TODO: Implement this method to send any registration to your app's servers.
+        registerDevice(refreshedToken);
     }
 
+    private void registerDevice(String token)
+    {
+
+        FirebaseUser firebaseUser;
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        String UID = firebaseUser.getUid();
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        HashMap<String,String> hashMap = new HashMap<>();
+
+        hashMap.put("deviceId",UID);
+        hashMap.put("deviceToken",token);
+
+        Call<ResponseBody> call = apiService.registerDevice(hashMap);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response)
+            {
+                if (response.code() == ApiInterface.RESPONSE_SUCCESS || response.code() == ApiInterface.NO_DATA_ON_SERVER)
+                {
+                    Toast.makeText(MyInstanceIDListenerService.this, "Working correctly", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MyInstanceIDListenerService.this, "Not working", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(MyInstanceIDListenerService.this, "Failure", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 
 }
---
+++ app/src/main/java/com/fabuleux/wuntu/billstore/Network/ApiInterface.java
@@ -0,0 +1,26 @@
+package com.fabuleux.wuntu.billstore.Network;
+
+import java.util.HashMap;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiInterface
+{
+
+    int INTERNAL_SERVER_ERROR = 500;
+    int UNAUTHORIZED = 401;
+    int INTERNET_FAILURE = 600;
+    int RESPONSE_SUCCESS = 200;
+    int NO_DATA_ON_SERVER = 204;
+    int BAD_REQUEST = 400;
+    int SERVER_DOWN = 503;
+
+    @POST("registerDevice")
+    Call<ResponseBody> registerDevice(@Body HashMap<String,String> hashMap);
+
+    @POST("deviceNotification")
+    Call<ResponseBody> sendNotification(@Body HashMap<String,String> hashMap);
+}
